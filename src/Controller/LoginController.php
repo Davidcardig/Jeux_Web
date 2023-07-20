0a49b9b51da045093f03fe181ffb6b4b34d1b2b3
Davidcardig <davidcardigos@hotmail.fr>
2023-07-20 20:14:27 +0200

    Version Database and New jeu in variable $jeux and display in vue index.html.twig

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,13 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response{
+
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+        $form = $this->createForm(UserType::class,$user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword($user,$user->getPassword())
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Vous êtes bien inscrit sur LocaJeu !'
+            );
+
+            return $this->redirectToRoute('app_login');
+
+        }
+
+        return $this->renderForm('registration/index.html.twig', [
+            'form' => $form
+        ]);
+
+    }
+
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
